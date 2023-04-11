8aa5a6d1de75e69bad19bfa48a36eaca811f7202
Anas Misbah <anas.misbahuddin4869@gmail.com>
2023-04-11 14:32:07 +0800

    add products

--- lib/app/modules/add_product/controllers/add_product_controller.dart
+++ lib/app/modules/add_product/controllers/add_product_controller.dart
@@ -1,23 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class AddProductController extends GetxController {
-  //TODO: Implement AddProductController
+  TextEditingController codeC = TextEditingController();
+  TextEditingController nameC = TextEditingController();
+  TextEditingController qtyC = TextEditingController();
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  RxBool isLoading = false.obs;
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  Future<Map<String, dynamic>> addProduct(Map<String, dynamic> data) async {
+    try {
+      DocumentReference<Map<String, dynamic>> res =
+          await firestore.collection("products").add(data);
+      await firestore.collection("products").doc(res.id).update(
+        {"productId": res.id},
+      );
+      return {"error": false, "message": "Berhasil menambah product"};
+    } catch (e) {
+      print(e);
+      return {"error": true, "message": "Tidak dapat menambah product"};
+    }
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/add_product/views/add_product_view.dart
+++ lib/app/modules/add_product/views/add_product_view.dart
@@ -13,11 +13,85 @@ class AddProductView extends GetView<AddProductController> {
         title: const Text('AddProductView'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'AddProductView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: ListView(
+        padding: EdgeInsets.all(20),
+        children: [
+          TextField(
+            autocorrect: false,
+            controller: controller.codeC,
+            keyboardType: TextInputType.number,
+            maxLength: 10,
+            decoration: InputDecoration(
+              labelText: "Product Code",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          TextField(
+            autocorrect: false,
+            controller: controller.nameC,
+            keyboardType: TextInputType.text,
+            decoration: InputDecoration(
+              labelText: "Name",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          TextField(
+            autocorrect: false,
+            controller: controller.qtyC,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Qty",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              if (controller.isLoading.isFalse) {
+                controller.isLoading.value = true;
+                if (controller.codeC.text.isNotEmpty &&
+                    controller.nameC.text.isNotEmpty &&
+                    controller.qtyC.text.isNotEmpty) {
+                  Map<String, dynamic> res = await controller.addProduct({
+                    "code": controller.codeC.text,
+                    "name": controller.nameC.text,
+                    "qty": int.tryParse(controller.qtyC.text) ?? 0,
+                  });
+                  Get.back();
+                  Get.snackbar(res['error'] == true ? "Error" : "Berhasil",
+                      res['message']);
+                } else {
+                  Get.snackbar('Error', 'Code, Name dan qty wajib diiisi');
+                }
+                controller.isLoading.value = false;
+              }
+            },
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              padding: EdgeInsets.symmetric(vertical: 20),
+            ),
+            child: Obx(
+              () => Text(
+                  controller.isLoading.isFalse ? "Add Product" : "Loading..."),
+            ),
+          ),
+        ],
       ),
     );
   }
